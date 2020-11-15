41e91f1df36f7a1c52c12bbbb470ae1b401dc2cd
Adriano Salles <adrianogsallesf@gmail.com>
2020-11-15 18:21:19 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+function imgSlider(event) {
+  document.querySelector('.html').src = event
+}
+
+function changeCircleColor(event) {
+  const circle = document.querySelector('.circle')
+  circle.style.background = event
+}
